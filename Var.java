23c3aee579864ee98f49da9d06f301df9323e72a
Afonya Muksunov <muks20@bk.ru>
2021-03-18 16:21:05 +0900

    all java files were added to the project

---
+++ Var.java
@@ -0,0 +1,9 @@
+public class Var {
+   public static void main (String[] args){
+     //System.out.println("Hello World!");
+     int age = 20;
+     float d = 75.52f;
+     System.out.println("Значение переменной age равно " + age);
+     System.out.print("Значение переменной d равно " + d);
+ }
+}

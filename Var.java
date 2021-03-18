d6bee783cbeecc8df212ba5a10a757698fbe055c
Afonya Muksunov <muks20@bk.ru>
2021-03-18 17:04:18 +0900

    added hello git! to Var.java

--- Var.java
+++ Var.java
@@ -4,6 +4,7 @@ public class Var {
      int age = 20;
      float d = 75.52f;
      System.out.println("Значение переменной age равно " + age);
-     System.out.print("Значение переменной d равно " + d);
+     System.out.println("Значение переменной d равно " + d);
+	 System.out.print("Hello Git");
  }
 }
